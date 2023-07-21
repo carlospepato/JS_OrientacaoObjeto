c19f7524c12dd0565065aec4548cd0724a23c745
Carlos Pepato <kadubelber1@gmail.com>
2023-07-20 22:13:23 -0300

    usandoCall

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -1,10 +1,59 @@
-const user = {
-    nome : 'Carlos',
-    email : 'kadubelber1@gmail.com',
-    nascimento : '1997/06/30',
-    role : 'admin',
-    ativo : true,
-    exibirinfos: function(){
+// const user = {
+//     nome : 'Carlos',
+//     email : 'carlos@gmail.com',
+//     nascimento : '1994/08/25',
+//     role : 'admin',
+//     ativo : true,
+//     exibirinfos: function(){
+//         console.log(this.nome, this.email)
+//     }
+// }
+
+//user.exibirinfos()
+// isso não vai funcionar ---> const exibir = user.exibirinfos()
+
+// const exibir = function(){
+//     console.log(this.nome)
+// }
+
+//const exibirNome = exibir.bind(user)
+// bind é usado para prender um objeto a um contexto
+//exibirNome()
+//exibir()
+
+//-------------------------------------------------//
+
+// function exibeInfos(){
+//     console.log(this.nome, this.email)
+// }
+
+// const user = {
+//     nome : 'Carlos',
+//     email: "carlos@carlos"
+// }
+
+// exibeInfos.call(user)
+
+//----------------------------------------------//
+
+function user(nome, email){
+    this.nome = nome
+    this.email = email
+
+    this.exibeInfos = function(){
         console.log(this.nome, this.email)
     }
 }
+
+const newUser = new user("Carlos", "carlos2@carlos")
+
+
+
+const outroUser = {
+    nome : 'Rodrigo',
+    email : 'rodrigo@rodrigo'
+}
+
+newUser.exibeInfos()
+newUser.exibeInfos.call(outroUser)
+
